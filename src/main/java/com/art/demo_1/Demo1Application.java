da6d06be267d332659784ed3b7d15600ca0e6f8d
Artsiom Voranau <artsiom.voranau@itechart-group.com>
2021-05-26 14:15:33 +0300

    dev: add text dev2_branch twice

--- src/main/java/com/art/demo_1/Demo1Application.java
+++ src/main/java/com/art/demo_1/Demo1Application.java
@@ -9,6 +9,7 @@ public class Demo1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 		System.out.println("Dev2 branch");
+		System.out.println("Dev2 branch");
 
 	}
 
